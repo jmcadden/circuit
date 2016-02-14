11248cc214e7319206ce5cd287b672880ed20e76
Jim Cadden <jmcadden@bu.edu>
2016-02-13 23:26:09 -0500

    Update dockerfile ubuntu and point local repo

--- misc/docker/build.sh
+++ misc/docker/build.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-mkdir -p /go
-cd /go
-hg clone https://code.google.com/p/go /go/golang
-cd /go/golang/src
-./all.bash
-
 echo "export ADDRESS=228.8.8.8:8788" >> /etc/profile
 echo "export GOPATH=/go/workspace" >> /etc/profile
 echo "export PATH=$PATH:/go/golang/bin:/go/workspace/bin:/go/util" >> /etc/profile
@@ -14,4 +8,4 @@ export PATH=$PATH:/go/golang/bin:/go/workspace/bin:/go/util
 
 mkdir -p /go/workspace/src
 
-go get github.com/gocircuit/circuit/cmd/circuit
+go get github.com/jmcadden/circuit/cmd/circuit
